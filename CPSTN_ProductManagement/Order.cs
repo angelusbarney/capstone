75a224f163a3388ea4f1d01cd39dbba791a335bb
AngelusBarney <angelusbarney@gmail.com>
2015-03-11 10:40:32 -0300

    First Commit

---
+++ CPSTN_ProductManagement/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSTN_ProductManagement
+{
+    public class Order : dbColumn
+    {
+        ProductDataSetTableAdapters.ProductOrderTableAdapter dbAdapter;
+        objectCounter listedData;
+        public void getProperties()
+        {
+            ProductDataSet.ProductOrderDataTable data = dbAdapter.GetData();
+
+            this.client = listedData.fetchClient(data.FindById(this.id).Client);
+            this.product = listedData.fetchProduct(data.FindById(this.id).Product);
+        }
+        public void setProperties()
+        {
+            ProductDataSet.ProductOrderDataTable data = dbAdapter.GetData();
+            data.AcceptChanges();
+        }
+        public Order(objectCounter list, int ID)
+        {
+            listedData = list; 
+            this._id = ID; 
+        }
+
+        public Order()
+        {
+
+        }
+
+        protected Product _product;
+        public Product product
+        {
+            get { return _product; }
+            set { _product = value; }
+        }
+        protected Client _client;
+        public Client client
+        {
+            get { return _client; }
+            set { _client = value; }
+        }
+    }
+}
---
+++ CPSTN_ProductManagement/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSTN_ProductManagement
+{
+    public class Product : dbColumn
+    {
+        public Product(objectCounter list, int ID)
+        {
+            listedData = list;
+            this._id = ID; 
+        }
+
+        ProductDataSetTableAdapters.ProductTableAdapter dbAdapter;
+        ProductDataSetTableAdapters.ProductSupplyTableAdapter dbSupplyAdapter;
+        objectCounter listedData;
+        public void getProperties()
+        {
+            ProductDataSet.ProductDataTable data = dbAdapter.GetData();
+            ProductDataSet.ProductSupplyDataTable dataSupply = dbSupplyAdapter.GetData();
+            this.name = data.FindById(this.id).Name;
+        }
+        public void setProperties()
+        {
+            ProductDataSet.ProductDataTable data = dbAdapter.GetData();
+            data.FindById(this.id).Name = this.name;
+            data.AcceptChanges();
+        }
+
+        protected List<Supply> supplies;
+        public void addSupply(Supply value)
+        {
+            supplies.Add(value);
+        }
+        public Supply getSupply(int index)
+        {
+            try
+            {
+                return supplies.ElementAt(index);
+            } 
+                catch(Exception e) 
+            {
+                return null;
+            }
+        }
+
+
+        public Product()
+        {
+
+        }
+    }
+}
