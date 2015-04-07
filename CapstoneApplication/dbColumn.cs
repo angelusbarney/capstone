9ff70a35bc63da38a0f59d8bf426cdda2526710b
AngelusBarney <angelusbarney@gmail.com>
2015-04-07 10:14:51 -0300

    Capstone James v04072015
    Current Version

---
+++ CapstoneApplication/dbColumn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneApplication
+{
+    abstract class dbColumn
+    {
+        protected String _name;
+        public String name { get { return _name; } set { _name = value; } }
+    }
+}
